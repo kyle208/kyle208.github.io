04173556976983c20816361cdbaa251bcb77b0df
kyle208 <847163260@qq.com>
2019-03-05 00:24:54 +0800

    Site updated: 2019-03-05 00:24:54

--- dist/music.js
+++ dist/music.js
@@ -15,6 +15,12 @@ const ap = new APlayer({
         url: "http://www.ytmp3.cn/down/55362.mp3",
         cover: "https://ss2.baidu.com/6ONYsjip0QIZ8tyhnq/it/u=2263725527,2286386301&fm=58&bpow=882&bpoh=890",
       },
+	  {
+        name: 'smile',
+        artist: "艾薇儿",
+        url: "http://www.ytmp3.cn/down/51089.mp3",
+        cover: "https://ss2.baidu.com/6ONYsjip0QIZ8tyhnq/it/u=4244291543,4144447772&fm=58&bpow=1280&bpoh=1280",
+      },
       {
         name: '风筝误',
         artist: '刘珂矣',
